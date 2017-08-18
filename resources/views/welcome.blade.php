69d19aa35d758309ccd28a50eb07f0712dd14b3c
UlrikeEder <ulrike.anna.eder@gmail.com>
2017-08-18 11:31:47 +0100

    changed the title to welcome to Bahrain

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Bahrain!
                 </div>
 
                 <div class="links">
